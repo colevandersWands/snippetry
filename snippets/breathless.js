cf14d0537758d25f531a5879bff601df4adb625d
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2023-08-05 21:40:16 +0200

    more be's, emojis in snips, snippeting

--- snippets/breathless.js
+++ snippets/breathless.js
@@ -2,3 +2,5 @@
   const breath = setTimeout(breathless, (slowness *= slowerness), slowness);
   console.log(['hold', 'breath in', 'hold', 'breath out'][breath % 4]);
 })();
+
+// tags: wellbeing
---
+++ snippets/emoj.draft.mjs
@@ -0,0 +1,11 @@
+// for funsole
+
+// prettier-ignore
+export const emoj = (text = '') => {
+  if (typeof text != 'string') console.log(text);
+  else console.log( Object.entries({
+      chicken: '🐔', egg:'🥚', belgium: '🇧🇪' // ...
+    }).reduce((emojied, [word, emoji]) => emojied.replace(new RegExp(word,'gi'), emoji), text));
+};
+
+export default emoj;
