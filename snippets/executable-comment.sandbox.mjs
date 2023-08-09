03cc51a2cdc3d2ae9d610f03b76b241c3082fb45
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2023-08-09 18:29:45 +0200

    reality check: aside

--- snippets/executable-comment.sandbox.mjs
+++ snippets/executable-comment.sandbox.mjs
@@ -1,12 +1,12 @@
-import { Spoken, unspoken as _ } from './executable-comment.mjs';
+import { Spoken, unspoken as _aside_ } from './executable-comment.mjs';
 
-Spoken.Create.new.characters.in.a.timeout.so.voices.can.load;
+Spoken.Create.new.characters.in.a.timeout.for.voices.to.load;
 
-setTimeout(() => {
+setTimeout(function realityCheck() {
   const __Robert__ = new Spoken({ voice: 'Organ', pitch: 2, rate: 0.5 });
   const __Alice___ = new Spoken({ pitch: 2 });
 
   __Robert__.Am.I.a.frenchman.from.Quebec?._;
-  __Alice___.No.Robert, _.Why.why.why?.__.Every.day['!'];
-  __Robert__.Awww.damn(':(').that.was.always.my.dream;
+  __Alice___.No.Robert, _aside_.Why.why.why?.__.Every.day['!'];
+  __Robert__.Awww.damn(':(').__.There.go.my.dreams;
 }, 50);
