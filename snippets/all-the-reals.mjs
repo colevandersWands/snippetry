f833c417f1371a50c2b59180508769623e0ca50c
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2023-08-23 07:25:34 +0200

    reads better

--- snippets/all-the-reals.mjs
+++ snippets/all-the-reals.mjs
@@ -7,18 +7,24 @@ const one = new Element(1);
 
 reals.add(zero);
 reals.add(one);
+addRealsBetween(zero, one);
 
-const allTheRealsBetween = (left, right) => {
+cantorize(reals);
+
+console.log(...reals);
+
+// --- --- --- --- --- ---
+
+function addRealsBetween(left, right) {
   const middle = new Element((left.getValue() + right.getValue()) / 2);
   if (!reals.has(middle)) {
     reals.add(middle);
-    allTheRealsBetween(left, middle);
-    allTheRealsBetween(middle, right);
+    addRealsBetween(left, middle);
+    addRealsBetween(middle, right);
   }
-};
-allTheRealsBetween(zero, one);
+}
 
-const cantorize = (incomplete, i = 0) => {
+function cantorize(incomplete, i = 0) {
   const numberToCheck = incomplete.getByOrder(i).getValue();
   numberToCheck[i] = numberToCheck[i] + 1;
   const maybeMissing = new Element(numberToCheck);
@@ -26,7 +32,4 @@ const cantorize = (incomplete, i = 0) => {
     incomplete.add(maybeMissing);
     cantorize(incomplete, i++);
   }
-};
-cantorize(reals);
-
-console.log(...reals);
+}
