3023d37c5b875d0960305330331680cb69640b57
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2025-01-18 18:58:43 -0500

    findable origin story

--- public/src/CONSTANTS.js
+++ public/src/CONSTANTS.js
@@ -2,6 +2,8 @@ export const API_URL = 'https://snippetry.onrender.com/api';
 
 export const GRAPH_URL = '';
 
+export const ORIGIN_STORY = './origin-story.html'
+
 export const REPO = 'https://github.com/colevandersWands/snippetry';
 
 export const SEPARATOR = '-=-=-=-';
--- public/src/init/outlinks.js
+++ public/src/init/outlinks.js
@@ -1,5 +1,8 @@
-import { API_URL, GRAPH_URL } from '../CONSTANTS.js';
+import { API_URL, GRAPH_URL, ORIGIN_STORY } from '../CONSTANTS.js';
 
-document.getElementById('api').href = API_URL;
+// document.getElementById('api').href = API_URL;
+
+document.getElementById('origin-story').href = ORIGIN_STORY;
+
 
 // document.getElementById('graph').href = GRAPH_URL;
