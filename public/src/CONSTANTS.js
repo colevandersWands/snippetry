c4630f890cc131a069a438640ec8252fff854fb2
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2025-01-18 19:32:16 -0500

    api: upgrade so update

--- public/src/CONSTANTS.js
+++ public/src/CONSTANTS.js
@@ -1,8 +1,10 @@
 export const API_URL = 'https://snippetry.onrender.com/api';
 
+export const BLANK_PAGE = './?about:blank';
+
 export const GRAPH_URL = '';
 
-export const ORIGIN_STORY = './origin-story.html'
+export const ORIGIN_STORY = './origin-story.html';
 
 export const REPO = 'https://github.com/colevandersWands/snippetry';
 
