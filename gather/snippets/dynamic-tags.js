4574e91acb90e029a6f816b8c89b80e80f1327d7
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2025-04-30 08:45:40 -0400

    now they comes, .docs drafting

--- gather/snippets/dynamic-tags.js
+++ gather/snippets/dynamic-tags.js
@@ -95,6 +95,16 @@ export const dynamicTags = [
     }
   },
 
+  function docs(snippets = []) {
+    for (const snippet of snippets) {
+      if (!snippet.title.toLowerCase().includes('.docs.')) null;
+      // find what it documents
+      // if there's nothing to find, it's not docs
+      // if there is, then indicate that it has docs
+      snippet.tags.push('metappet', 'docs');
+    }
+  },
+
   // function translation(snippets = []) {
   //   const lang = (name = '') => {
   //     const ext = name.split('.').pop().toLowerCase();
