4e082eee42c5ea6dc47d577fea7d6f49551fc108
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2025-04-30 08:45:40 -0400

    now they comes, .docs drafting

--- public/lib/scheme-dot-json/reserved.js
+++ public/lib/scheme-dot-json/reserved.js
@@ -1,17 +1,18 @@
-import { environment } from "./environment.js";
+import { environment } from './environment.js';
 
 export const reserved = {
-  if: "if",
-  cond: "cond",
-  else: "else",
-  define: "define",
-  comment: ";",
-  commentProgram: ";;",
-  pipeLog: "*log",
-  debug: "*debug",
-  list: "list",
+  if: 'if',
+  cond: 'cond',
+  else: 'else',
+  define: 'define',
+  comment: ';',
+  commentProgram: ';;',
+  pipeLog: '*log',
+  debug: '*debug',
+  list: 'list',
+
   ...Object.keys(environment).reduce(
     (all, next) => ({ ...all, [next]: environment[next] }),
-    {}
+    {},
   ),
 };
